81d138ec7b9ef6ae0f1af17dfcd966cf918d5b89
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-10-05 16:18:26 -0300

    feat: adiciona botão de logout ao menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
 import { MenuItemComponent } from './components/menu/menu-item/menu-item.component';
 import { MenuLogoComponent } from './components/menu/menu-logo/menu-logo.component';
+import { LogoutButtonComponent } from './components/menu/logout-button/logout-button.component';
 
 @NgModule({
   declarations: [
     MenuComponent,
     MenuItemComponent,
     MenuLogoComponent,
+    LogoutButtonComponent,
   ],
   imports: [
     CommonModule,
