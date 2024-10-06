5f67ca17c66357e70801d826732d24d2fa749944
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-10-06 16:02:01 -0300

    refact: adiciona inputs

--- src/app/shared/components/menu-profile/menu-profile.component.ts
+++ src/app/shared/components/menu-profile/menu-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-profile',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './menu-profile.component.scss'
 })
 export class MenuProfileComponent {
-
+  @Input({ required: true }) profileName: string = '';
+  @Input() image: string = '';
 }
