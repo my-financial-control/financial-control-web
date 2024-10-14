2c0b95c1012ae747833c5eb1bccf8845d4751dbc
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-10-14 20:18:55 -0300

    feat: adiciona componente balance card

--- src/app/pages/overview/overview.module.ts
+++ src/app/pages/overview/overview.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview.component';
 import { SharedModule } from '../../shared/shared.module';
+import { BalanceCardComponent } from './components/total-balance-card/balance-card.component';
+import { AccountCardComponent } from './components/account-card/account-card.component';
+import { SliderFooterComponent } from './components/slider-footer/slider-footer.component';
 
 @NgModule({
   declarations: [
     OverviewComponent,
+    BalanceCardComponent,
+    AccountCardComponent,
+    SliderFooterComponent,
   ],
   imports: [
     CommonModule,
